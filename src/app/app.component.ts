99cf8fc8ce27103d6e9a724b6b366ebadac22695
saritagore21@gmail.com <rahulgore34@gmail.com>
2024-12-07 17:14:21 +0530

    api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng19demo';
+  service = inject(DataService)
 
-  
+  ngOnInit(): void {
+    this.service.getData('dev').subscribe(dev => {
+      console.log(dev);
+      
+    })
+  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  url = 'http://localhost:3000/';
+  http =  inject(HttpClient);
   constructor() { }
+
+  getData(str: string): Observable<any> {
+
+    return this.http.get(`${this.url}${str}`);
+  }
 }
