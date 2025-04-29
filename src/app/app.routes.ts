01bc8e54a7dc40783ce5e50f6042b0b5280bbda7
saritagore21@gmail.com <rahulgore34@gmail.com>
2025-04-29 15:42:15 +0530

    handle empty path route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
-        path: 'login', component:LoginComponent
+        path: '', redirectTo: 'home', pathMatch: 'full'
     },
-{
-    path:'dashboard', component: DashboardComponent
-}
+    {
+  path: 'home', component: HomeComponent
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'dashboard', component: DashboardComponent
+    }
 ];
